8efb19a98af2f7b2f95d57d6270b0d0596028981
Clara Rojas Levano <milagrosrojas730@gmail.com>
2025-02-06 00:19:04 -0500

    Mi solución para el reto

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/milagrosrojas730.py
@@ -0,0 +1,27 @@
+#https://python.org
+
+#Comentario
+
+"""
+Comentario de 3 comillas
+
+"""
+
+#Otro tipo
+#de
+#comentario
+
+may = "Soy una variable"
+may = "Python es un lenguaje"
+
+# "Constante" (Python no tiene constantes nativas)
+
+cadena = "Hola, mundo"    # String
+entero = 42               # Int
+decimal = 3.14            # Float
+booleano = True           # Booleano 
+lista = [1, 2, 3]         # mutable
+tupla = (4, 5, 6)         # Tupla 
+nulo = None               # Valor nulo 
+
+print(f"¡Hola, {Python}!")
